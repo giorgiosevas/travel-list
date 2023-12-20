0f295a8663f594a4ca07f387f33a2c8a6e08f2db
George Sevastiadis <gsevasti@netcompany-intrasoft.com>
2023-12-20 13:46:29 +0200

    rendering the items list

--- src/packingList.js
+++ src/packingList.js
@@ -4,6 +4,35 @@ import React, { useState } from 'react';
 //CSS imports
 import './index.css';
 
+const initialItems = [
+	{ id: 1, description: 'Passports', quantity: 2, packed: false },
+	{ id: 1, description: 'Charger', quantity: 1, packed: true },
+	{ id: 2, description: 'Socks', quantity: 12, packed: false }
+];
+
+const Item = ({ item }) => {
+	return (
+		<li>
+			<span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+				{item.quantity}
+				{item.description}
+			</span>
+			<button>❌</button>
+		</li>
+	);
+};
+
 export const PackingList = () => {
-	return <div className="list">LIST</div>;
+	return (
+		<div className="list">
+			<ul>
+				{initialItems.map((item) => (
+					<Item
+						key={item.id}
+						item={item}
+					/>
+				))}
+			</ul>
+		</div>
+	);
 };
