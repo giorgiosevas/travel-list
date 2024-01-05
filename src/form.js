7d4a1b21f37f6def2c86d98ddec646c00d105b47
George Sevastiadis <gsevasti@netcompany-intrasoft.com>
2024-01-05 15:45:31 +0200

    example for controlled elements

--- src/form.js
+++ src/form.js
@@ -5,9 +5,21 @@ import React, { useState } from 'react';
 import './index.css';
 
 export const Form = () => {
+	const [description, setDescription] = useState('');
+	const [quantity, setQuantity] = useState('');
+
 	const handleSubmit = (e) => {
 		e.preventDefault(); //->to prevent the page reloading, cause it is pure HTML from the <form>
-		console.log(e);
+		//console.log(e);
+
+		if (!description) return;
+
+		const newItem = { description, quantity, packed: false, id: Date.now() };
+		console.log('newItem->', newItem);
+
+		//We write this to put the 2 fields, select+input, to their initial state.
+		setDescription('');
+		setQuantity(1);
 	};
 
 	return (
@@ -16,7 +28,18 @@ export const Form = () => {
 			onSubmit={handleSubmit}
 		>
 			<h3>What do you need for your 😍 trip?</h3>
-			<select>
+			<select
+				value={quantity}
+				//? here the select is a string:
+				//? onChange={(e) => {
+				//? 	setQuantity(e.target.value);
+				//? }}
+
+				//? here the select value is a number:
+				onChange={(e) => {
+					setQuantity(Number(e.target.value));
+				}}
+			>
 				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
 					<option
 						value={num}
@@ -29,6 +52,11 @@ export const Form = () => {
 			<input
 				type="text"
 				placeholder="Item..."
+				value={description}
+				onChange={(e) => {
+					//console.log(e.target.value);
+					setDescription(e.target.value);
+				}}
 			></input>
 			<button>Add</button>
 		</form>
